5daf394a7873b2997c018355a1f621314d3c754e
Kai Lennart Drewes <stu218960@mail.uni-kiel.de>
2021-04-03 07:52:25 +0200

    embedded entropia precision/fitness evaluation

--- e_Evaluation/e_entropia.py
+++ e_Evaluation/e_entropia.py
@@ -1,19 +1,13 @@
 from subprocess import Popen, PIPE, STDOUT
 
-def compute_entropia(variant, xesFile, pnmlFile):
 
-    args = ['java', '-jar', 'jbpt-pm-entropia-1.6.jar', f'-{variant}', '-srel=3', '-sret=3', f'-rel={xesFile}', f'-ret={pnmlFile}', '--silent'] 
+def compute_entropia(variant, xesFile, pnmlFile):
+    args = ['java', '-jar', 'e_Evaluation/jbpt-pm-entropia-1.6.jar', f'-{variant}', '-srel=3', '-sret=3', f'-rel={xesFile}',
+            f'-ret={pnmlFile}', '--silent']
     p = Popen(args, stdout=PIPE, stderr=STDOUT)
 
     res = ""
     for line in p.stdout:
         res = res + str(line)
-    
+
     return res
-
-
-precision = compute_entropia("pmp", xesFile = "log_export.xes", pnmlFile ="petri_net.pnml")
-print(f'Precision {precision}')
-
-recall = compute_entropia("pmr", xesFile = "log_export.xes", pnmlFile ="petri_net.pnml")
-print(f'Recall {recall}')
